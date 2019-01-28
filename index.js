a407bc6abc000c144b42edb37083d2b0a9e1f2f6
FreitagDavid <freitagdavid@outlook.com>
2019-01-28 18:42:30 -0500

    index: Implemented repair and fail

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 exports.success = item => {
-    item.enhancement += 1;
-    item.name = changeName(item.name, item.enhancement);
-    return item;
+    const workingItem = Object.assign({}, item);
+    if (workingItem.enhancement < 15 && item.durability < 25) {
+        return workingItem;
+    }
+    if (workingItem.enhancement > 14 && item.durability < 10) {
+        return workingItem;
+    }
+    workingItem.enhancement += 1;
+    workingItem.name = changeName(workingItem.name, workingItem.enhancement);
+    return workingItem;
+};
+
+exports.fail = item => {
+    const workingItem = Object.assign({}, item);
+    if (workingItem.enhancement < 15) {
+        workingItem.durability -= 5;
+    } else if (workingItem.enhancement > 14) {
+        workingItem.durability -= 10;
+    }
+    if (workingItem.enhancement > 16) {
+        workingItem.enhancement -= 1;
+        console.log(workingItem.enhancement);
+        workingItem.name = changeName(
+            workingItem.name,
+            workingItem.enhancement
+        );
+    }
+    return workingItem;
+};
+
+exports.repair = item => {
+    const workingItem = Object.assign({}, item);
+    workingItem.durability = 100;
+    return workingItem;
 };
 
 function changeName(name, enhancement) {
+    console.log(name, enhancement);
     if (name.match(/\[.*\]/g)) {
-        name = item.name.match(/(\[.*\])(.*)/g)[1];
+        name = name.match(/(\[.*\])(.*)/g)[1];
     }
     switch (enhancement) {
         case 16:
